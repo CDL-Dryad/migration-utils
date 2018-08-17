3cfea34eeef941615ec19b28f75e87f3ee8f1e82
daisie_local <daisieh@gmail.com>
2018-08-17 16:58:48 -0700

    add new user if no match

--- import_epersons_to_dash.py
+++ import_epersons_to_dash.py
@@ -3,6 +3,7 @@
 import optparse
 import re
 import os
+import datetime
 
 
 # Use bash dryad-utils/aws-tools/psql_query.sh 'select eperson_id, email, firstname, lastname from eperson'
@@ -59,16 +60,20 @@ def main():
     with open(options.eperson_file) as f:
         epersons = f.read().splitlines()
     
+    timestamp = datetime.datetime.now().replace(microsecond=0).isoformat(' ')
+    
     for eperson in epersons:
         eperson_parts = eperson.split("\t")
         print eperson_parts[1]
         eperson_dict = dict_from_query('select * from dash.stash_engine_users where email = \'%s\'' % eperson_parts[1])
         if eperson_dict is not None and eperson_dict['email'] == eperson_parts[1]:
             result = sql_query('update dash.stash_engine_users set eperson_id = \'%s\' where email = \'%s\'' % (eperson_parts[0], eperson_parts[1]))
-            if result.close() is None:
-                print "SUCCESS"
-            else:
-                print "ERROR"
+        else:
+            result = sql_query('insert into dash.stash_engine_users (first_name, last_name, email, created_at, updated_at, tenant_id, role, eperson_id) values (\'%s\', \'%s\', \'%s\', \'%s\', \'%s\', \'dryad\', \'user\', \'%s\')' % (eperson_parts[2], eperson_parts[3], eperson_parts[1], timestamp, timestamp, eperson_parts[0]))
+        if result.close() is None:
+            print "\tSUCCESS"
+        else:
+            print "\tERROR"
 
 
 if __name__ == '__main__':
